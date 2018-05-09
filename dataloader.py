e1c426a290702d24ba4a5c5238547cbfadeb8164
Jason Hu <31903987+jason-hu7@users.noreply.github.com>
2018-05-08 22:15:13 -0700

    unalignedd option implemented

--- dataloader.py
+++ dataloader.py
@@ -6,49 +6,55 @@ from PIL import Image
 
 class GANDataset(Dataset):
 
-	# Initial logic here, including reading the image files and transform the data
-	def __init__(self, rootA, rootB, transform=None):
-		# initialize image path and transformation
-		self.image_pathsA = list(map(lambda x: os.path.join(rootA, x), os.listdir(rootA)))
-		self.image_pathsB = list(map(lambda x: os.path.join(rootB, x), os.listdir(rootB)))
-		self.transform = transform
+    # Initial logic here, including reading the image files and transform the data
+    def __init__(self, rootA, rootB, transform=None, unaligned=False):
+        # initialize image path and transformation
+        self.image_pathsA = list(map(lambda x: os.path.join(rootA, x), os.listdir(rootA)))
+        self.image_pathsB = list(map(lambda x: os.path.join(rootB, x), os.listdir(rootB)))
+        self.transform = transform
+        self.unaligned = unaligned
 
-	# read the image and preprocess
-	def __getitem__(self, index):
-		image_pathA = self.image_pathsA[index]
-		image_pathB = self.image_pathsB[index]
+    # override to support indexing
+    def __getitem__(self, index):
+        
+        image_pathA = self.image_pathsA[index]
+        imageA = Image.open(image_pathA).convert('RGB')
 
-		imageA = Image.open(image_pathA).convert('RGB')
-		imageB = Image.open(image_pathB).convert('RGB')
+        # unaligned the paired images if needed
+        if self.unaligned:
+            image_pathB = self.image_pathsB[np.random.randint(0, len(self.image_pathsB)-1)]
+        else:
+            image_pathB = self.image_pathsB[index]
+            
+        imageB = Image.open(image_pathB).convert('RGB')
+        
+        # transform the images if needed
+        if self.transform is not None:
+            imageA = self.transform(imageA)
+            imageB = self.transform(imageB)
 
-		if self.transform is not None:
-			imageA = self.transform(imageA)
-			imageB = self.transform(imageB)
+        return imageA, imageB
 
-		return imageA, imageB
-
-	# returns the number of examples we read
-	def __len__(self):
-		return len(self.image_pathsA)  # of how many examples we have
+    # returns the number of examples we read
+    def __len__(self):
+        return max(len(self.image_pathsA), len(self.image_pathsB))  # of how many examples we have
 
 
-## return - DataLoader
-def get_dataloader(image_pathA, image_pathB, batch_size, image_size=(600, 600)):
-	transform = transforms.Compose([
-		# resize PIL image to given size
-		transforms.Resize(image_size),
-		# crop image at randomn location
-		transforms.RandomCrop(image_size),
-		# flip images randomly
-		transforms.RandomHorizontalFlip(),
-		# convert image input into torch tensor
-		transforms.ToTensor(),
-		# normalize image with mean and standard deviation
-		transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+## return - DataLoader, batch dimension in (batch_size, channel, H, W)
+def get_dataloader(image_pathA, image_pathB, batch_size, image_size=(256, 256), unaligned=False):
+    transform = transforms.Compose([
+        # resize PIL image to given size
+        transforms.Resize(image_size),
+        # crop image at randomn location
+        transforms.RandomCrop(image_size),
+        # flip images randomly
+        transforms.RandomHorizontalFlip(),
+        # convert image input into torch tensor
+        transforms.ToTensor(),
+        # normalize image with mean and standard deviation
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    
+    batch_dataset = GANDataset(image_pathA, image_pathB, transform, unaligned)
 
-	batch_dataset = GANDataset(image_pathA, image_pathB, transform)
-
-	#     B_dataloader = DataLoader(dataset=dataset['B'], batch_size=batch_size, shuffle=True)
-
-	return DataLoader(dataset=batch_dataset, batch_size=batch_size, shuffle=True)
+    return DataLoader(dataset=batch_dataset, batch_size=batch_size, shuffle=True)
 
