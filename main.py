1734769aeaac07616479bafee57ee10766f55e31
Weini <wnlscindy@gmail.com>
2018-05-14 15:15:04 -0700

    num_workers breaks. change default back to 0.

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ parser.add_argument('--print_every_val', default=200, type=int)
 parser.add_argument('--save_every_epoch', default=20, type=int)
 parser.add_argument('--eval_n', default=100, type=int, help='number of examples from val set to evaluate on each epoch')
 parser.add_argument('--save_n_img', default=5, type=int, help='number of images to save at test time')
-parser.add_argument('--num_workers', default=2, type=int)
+parser.add_argument('--num_workers', default=0, type=int)
 # Optimization
 parser.add_argument('--lr', default=0.0002, type=float)
 parser.add_argument('--wd', default=0, type=float)
